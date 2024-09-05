380e63c6142146474770da681f3e4408079da7c7
awschmeder <awschmeder@lbl.gov>
2024-09-05 16:57:16 -0700

    don't display errors in pip install

--- proxy/start-cborg-client-proxy.sh
+++ proxy/start-cborg-client-proxy.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
-pip install -r $SCRIPT_DIR/requirements.txt 1>/dev/null
+pip install -r $SCRIPT_DIR/requirements.txt 1>/dev/null 2>&1
 
 PYTHONPATH=$SCRIPT_DIR/proxy.py python $SCRIPT_DIR/cborgclient.py
 
