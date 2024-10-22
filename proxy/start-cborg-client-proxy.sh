7e3b3abb076ccc3a89ba02dcf85ae4bbc61f9074
your name <awschmeder@lbl.gov>
2024-10-22 15:17:25 -0700

    add additional echo statements for information

--- proxy/start-cborg-client-proxy.sh
+++ proxy/start-cborg-client-proxy.sh
@@ -2,11 +2,21 @@
 
 SCRIPT_DIR=$(cd "$(dirname '$0')" && pwd)
 
-git pull
+echo "Checking for updates..."
+cd $SCRIPT_DIR && git pull
 
+echo "Activate Virtual Environment"
 source $SCRIPT_DIR/venv/bin/activate
 
+echo "Python Version Check: "
+python3 -V
+
+echo "Start"
 python3 $SCRIPT_DIR/cborgclient.py
 
+echo "Deactivate"
 deactivate
+
+echo "Done"
+
 
